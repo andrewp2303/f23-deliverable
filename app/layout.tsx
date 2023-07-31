e241b345f9c46df414bd0f1745e81413735e6ea5
Matthew Su <matthewsu@college.harvard.edu>
2023-07-31 17:34:05 -0400

    changed components-navbar from route group to private folder:

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
-import { ModeToggle } from "@/app/(components-navbar)/mode-toggle";
+import { ModeToggle } from "@/app/_components-navbar/mode-toggle";
 import { Toaster } from "@/components/ui/toaster";
-import AuthStatus from "./(components-navbar)/auth-status";
-import Navbar from "./(components-navbar)/navbar";
+import AuthStatus from "./_components-navbar/auth-status";
+import Navbar from "./_components-navbar/navbar";
 import "./globals.css";
 import { Providers } from "./providers";
 
